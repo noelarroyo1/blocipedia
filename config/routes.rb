dcdf865a804feddd3606edf538c4880c6ed8d838
Noel Arroyo <noel.arroyo1@gmail.com>
2018-05-16 09:54:31 -0400

    revise user log-in and log-out

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
-
   get 'welcome/about'
+
+  resources :users, only: [:show]
   root to: 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
