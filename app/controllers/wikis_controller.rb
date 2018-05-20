856988454232ae83d839020005948cd9e1f6fdb6
Noel Arroyo <noel.arroyo1@gmail.com>
2018-05-19 20:55:35 -0400

    created a form partial, added strong_params, and deleted model table from db

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -11,36 +11,8 @@ class WikisController < ApplicationController
     @wiki = Wiki.new
   end
 
-  def create
-    @wiki = Wiki.new
-    @wiki.title = params[:wiki][:title]
-    @wiki.body = params[:wiki][:body]
-
-    if @wiki.save
-      flash[:notice] = "Wiki was saved."
-      redirect_to @wiki
-    else
-      flash.now[:alert] = "There was an error saving the wiki. Please try again."
-      render :new
-    end
-  end
-
   def edit
     @wiki = Wiki.find(params[:id])
-  end
-
-  def update
-    @wiki = Wiki.find(params[:id])
-    @wiki.title = params[:wiki][:title]
-    @wiki.body = params[:wiki][:body]
-
-    if @wiki.save
-      flash[:notice] = "Wiki was updated!"
-      redirect_to @wiki
-    else
-      flash.now[:alert] = "There was an error saving the wiki. Please try again."
-      render :edit
-    end
   end
 
   def destroy
@@ -52,6 +24,37 @@ class WikisController < ApplicationController
     else
       flash.now[:alert] = "There was an error deleting the wiki."
       render :show
-    end 
+    end
+  end
+
+  def update
+    @wiki = Wiki.find(params[:id])
+    @wiki.assign_attributes(wiki_params)
+
+    if @wiki.save
+      flash[:notice] = "Wiki was updated!"
+      redirect_to @wiki
+    else
+      flash.now[:alert] = "There was an error saving the wiki. Please try again."
+      render :edit
+    end
+  end
+
+  def create
+    @wiki = Wiki.new(wiki_params)
+
+    if @wiki.save
+      flash[:notice] = "Wiki was saved."
+      redirect_to @wiki
+    else
+      flash.now[:alert] = "There was an error saving the wiki. Please try again."
+      render :new
+    end
+  end
+
+private
+
+  def wiki_params
+    params.require(:wiki).permit(:title, :body)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,30 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20180516192353) do
 
-  create_table "models", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.integer "failed_attempts", default: 0, null: false
-    t.string "unlock_token"
-    t.datetime "locked_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_models_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
